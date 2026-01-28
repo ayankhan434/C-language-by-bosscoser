acd6820173876ff77f33d2dc6f330c1a7f78a6a7
ayan khan <ayankhanayan992@gmail.com>
2026-01-28 18:18:17 +0530

    beginning in c++

---
+++ DAY-02/sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () { 
+    int a,b;
+    cin >> a;
+    cin >> b;
+    int sum = a + b;
+    cout<<"the sum of two numbers is "<<sum<<endl;
+return 0;
+}
---
+++ DAY-02/sumof02.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int a,b,c;
+    cout << "enter the first number:";
+    cin >> a;
+    cout << "enter the second number:";
+    cin >> b;
+    cout << "enter the third number:";
+    cin >> c;
+    int sum = a + b + c;
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
